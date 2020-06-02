aebcf61d8accfb0c1f33389afa2680afdbed93ad
Giovanni <giovannigiampaolo58@gmail.com>
2020-06-02 19:40:19 +0200

    - fix to prevent blocked:mixed-content

--- src/components/utils/wather-consts.ts
+++ src/components/utils/wather-consts.ts
@@ -1,3 +1,3 @@
 export const API_FETCH_KEY = '53b8c510025423ce7d42791df96a29e2'
 
-export const FETCH_BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?q='
+export const FETCH_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?q='
