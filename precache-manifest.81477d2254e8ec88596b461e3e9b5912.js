267154795d4b7ed52e9f4f1a69bf41a1ecdd01d9
Giovanni <giovannigiampaolo58@gmail.com>
2020-06-02 19:25:17 +0200

    Updates

---
+++ precache-manifest.81477d2254e8ec88596b461e3e9b5912.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9e705088fcea9100945a14180456f1",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "3d5ec8777daeb4a7efbd",
+    "url": "/weather-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fe3a8590f449f391090a",
+    "url": "/weather-app/static/css/main.7dd0755c.chunk.css"
+  },
+  {
+    "revision": "3d5ec8777daeb4a7efbd",
+    "url": "/weather-app/static/js/2.67b1a704.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.67b1a704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe3a8590f449f391090a",
+    "url": "/weather-app/static/js/main.8f4fbe78.chunk.js"
+  },
+  {
+    "revision": "55676f7ecad5f9b6942e",
+    "url": "/weather-app/static/js/runtime-main.58bc7688.js"
+  }
+]);
